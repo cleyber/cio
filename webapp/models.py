5493d633bb8aa1af38aad481ffe6fccb1d5adf82
cleyber <cleyber.rengifo@f2x.com.co>
2017-05-25 16:59:43 -0500

    Working in models

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """docstring forUser."""
+    db_table = 'users'
+
+    ROLE = (
+        ('ad', 'Admin')
+        ('em', 'Employee')
+    )
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=128)
+    role = models.CharField(max_length=2, choices=ROLE)
+
+class Schedule(models.Model):
+    """docstring for Schedule"""
+    db_table = 'schedules'
+
+    date = models.DateField()
+    start = models.DateTimeField()
+    finish = models.DateTimeField()
+    userId = model.ForeignKey(User, on_delete=models.CASCADE)
+
+class Paysheet(models.Model)
+    """docstring for Paysheet"""
+    db_table = 'Paysheet'
+
+    total = models.DecimalField()
+    start = models.DateTimeField()
+    finish = models.DateTimeField()
